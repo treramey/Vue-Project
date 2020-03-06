3d8164c5c34875f72fbdc85289dd5bcc91d3bc25
Trevor Ramey <treramey@gmail.com>
2020-03-05 18:25:34 -0600

    made partical size bigger

--- package/src/shader/demo.vertex.glsl
+++ package/src/shader/demo.vertex.glsl
@@ -2,6 +2,6 @@ attribute float scale;
 void main() {
     //modelViewMatrix 
     vec4 mvPosition = modelViewMatrix * vec4( position, 0.5 );
-    gl_PointSize = scale * ( 250.0 / - mvPosition.z );
+    gl_PointSize = scale * ( 450.0 / - mvPosition.z );
     gl_Position = projectionMatrix * mvPosition;
 }
